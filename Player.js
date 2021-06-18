867349c9550d51dcc7726e6aab57b06d0f45f1a7
ArshiaMJoseph <80831208+ArshiaMJoseph@users.noreply.github.com>
2021-06-18 20:47:02 +0530

    Add files via upload

---
+++ Player.js
@@ -0,0 +1,37 @@
+class Player{
+    constructor(){
+        this.name = null;
+        this.distance = 0;
+        this.index = null;
+    }
+
+    update(){
+
+        var playerIndex = "players/player"+this.index;// to referinside the node i.e players/player1
+        database.ref(playerIndex).set({name:this.name,distance:this.distance});//to update the database with name and distance
+
+    }
+
+//get the playercount
+    updateCount(count){
+        database.ref('/').update({playerCount:count});
+
+    }
+
+
+    getCount(){
+        var playerCountRef = database.ref('playerCount').on("value",(data)=>{
+            playerCount = data.val();
+
+        })
+    }
+
+
+    static getPlayerInfo(){//when u call a function using class name , the funaction should be static function
+        var playerInfoRef = database.ref('players').on("value",(data)=>{
+            allPlayers = data.val();
+
+        })
+
+    }
+}
